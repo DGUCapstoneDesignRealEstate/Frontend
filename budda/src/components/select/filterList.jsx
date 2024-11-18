4544fb304343213249ca6f5fc5616458aae35f76
okiidokim <scw09268@dgu.ac.kr>
2024-11-18 22:45:23 +0900

    [FIX] 토글 선택 구-> 법정동 되게 수정
    ㅎ

--- budda/src/components/select/filterList.jsx
+++ budda/src/components/select/filterList.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import Filter from './filter';
-import FilterDuration from './filterDuration';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Filter from "./filter";
+import FilterDuration from "./filterDuration";
+import { arrayList, dongList, guList } from "./data";
 
 const FilterWrapper = styled.div`
   position: relative;
@@ -11,34 +12,30 @@ const FilterWrapper = styled.div`
 `;
 
 export default function FilterList() {
-  const arrayList = [
-    { name: '최신순' },
-    { name: '전용면적 넓은 순' },
-    { name: '매매가 높은 순' },
-    { name: '예측가 높은 순' },
-  ];
-  const guList = [
-    { name: '서울 전체' },
-    { name: '노원구' },
-    { name: '강남구' },
-    { name: '강서구' },
-    { name: '강동구' },
-    { name: '강북구' },
-    { name: '성북구' },
-    { name: '송파구' },
-    { name: '동대문구' },
-    { name: '서초구' },
-    { name: '송파구' },
-    { name: '중구' },
-  ];
-  const dongList = [{ name: '동 선택' }];
-  const aptName = [{ name: '아파트 선택' }];
-  const areaSelection = [{ name: '전용면적 선택' }];
+  const aptName = [{ name: "아파트 선택" }];
+  const areaSelection = [{ name: "전용면적 선택" }];
+
+  const [selectedGu, setSelectedGu] = useState(guList[0]); // 초기값: "서울 전체"
+  const [filteredDong, setFilteredDong] = useState(
+    dongList.filter((dong) => dong.guNum === guList[0].num)
+  );
+
+  useEffect(() => {
+    if (selectedGu) {
+      setFilteredDong(dongList.filter((dong) => dong.guNum === selectedGu.num));
+    }
+  }, [selectedGu]);
+
+  const handleGuChange = (selectedGuName) => {
+    const selectedGu = guList.find((gu) => gu.name === selectedGuName);
+    setSelectedGu(selectedGu || guList[0]); // 선택된 구가 없으면 기본값 설정
+  };
+
   return (
     <FilterWrapper>
       <Filter list={arrayList} />
-      <Filter list={guList} />
-      <Filter list={dongList} />
+      <Filter list={guList} onChange={handleGuChange} />
+      <Filter list={filteredDong} />
       <Filter list={aptName} width="240px" />
       <Filter list={areaSelection} />
       <FilterDuration />
