ce660b2fa9bd82e8c341a42342a21ed2aab1c299
okiidokim <scw09268@dgu.ac.kr>
2024-11-22 23:42:42 +0900

    [FIX] api는 아니지만 세부적 수정
    로고 및 사진 중복 처리

--- budda/src/pages/startPage/StartPage.jsx
+++ budda/src/pages/startPage/StartPage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Header from '../../components/header/header';
-import LogoHome from '../../components/logo/logoHome';
-import FooterHome from '../../components/footer/footerStart';
-import './startPage.css';
+import React from "react";
+import Header from "../../components/header/header";
+import LogoHome from "../../components/logo/logoHome";
+import FooterHome from "../../components/footer/footerStart";
+import "./startPage.css";
 
 export default function StartPage() {
   return (
